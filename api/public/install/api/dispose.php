ebd0a6a51209e14e51f0a807b4c283a2a3bace1b
dspurl <49799121+dspurl@users.noreply.github.com>
2021-01-13 12:14:36 +0800

    Update dispose.php

--- api/public/install/api/dispose.php
+++ api/public/install/api/dispose.php
@@ -11,9 +11,9 @@ switch ($_GET['step']){
     case 1: //第一步：安装数据表
         $shell.= sellCode('php artisan migrate');
         $return =[
-            'code'=>1,
-            'step'=>2,
-            'msg'=>PHP_EOL.'数据表安装成功'.PHP_EOL,
+            'code'=>0,
+            'msgCode'=>'未知错误',
+            'msg'=>$shell
         ];
         if(strstr($shell,"Unknown database")){
             $return=[
@@ -44,9 +44,9 @@ switch ($_GET['step']){
     case 2: //第二步：添加demo数据
         $shell.= sellCode('php artisan generate:demo');
         $return =[
-            'code'=>1,
-            'step'=>3,
-            'msg'=>$shell.PHP_EOL.'demo数据添加成功'.PHP_EOL,
+            'code'=>0,
+            'msgCode'=>'未知错误',
+            'msg'=>$shell
         ];
         if(strstr($shell,"Duplicate entry")){
             $return=[
@@ -59,9 +59,9 @@ switch ($_GET['step']){
     case 3: //第三步：生成APP_KEY
         $shell.= sellCode('php artisan key:generate');
         $return =[
-            'code'=>1,
-            'step'=>4,
-            'msg'=>$shell.PHP_EOL.'APP_KEY密钥生成成功'.PHP_EOL,
+            'code'=>0,
+            'msgCode'=>'未知错误',
+            'msg'=>$shell
         ];
         if(strstr($shell,"Application key set successfully")){
             $return=[
@@ -74,9 +74,9 @@ switch ($_GET['step']){
     case 4: //第四步：生成oauth密钥
         $shell.= sellCode('php artisan passport:keys --force');
         $return =[
-            'code'=>1,
-            'step'=>5,
-            'msg'=>$shell.PHP_EOL.'oauth密钥生成成功'.PHP_EOL,
+            'code'=>0,
+            'msgCode'=>'未知错误',
+            'msg'=>$shell
         ];
         if(strstr($shell,"Encryption keys generated successfully")){
             $return=[
@@ -89,9 +89,9 @@ switch ($_GET['step']){
     case 5: //第五步：生成OAuth令牌
         $env=file_get_contents("../../../.env");
         $return =[
-            'code'=>1,
-            'step'=>'end',
-            'msg'=>$shell.PHP_EOL.'OAuth令牌生成成功'.PHP_EOL,
+            'code'=>0,
+            'msgCode'=>'未知错误',
+            'msg'=>$shell
         ];
         if(!strstr($env,"PASSPORT_CLIENT_ID=1")){
             $shell.= sellCode('php artisan passport:client --password --provider=admins');
