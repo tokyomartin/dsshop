699275e791443bc2ee56a22866706f754768897a
dspurl <49799121+dspurl@users.noreply.github.com>
2021-01-13 17:01:15 +0800

    1.4.2更新

---
+++ api/public/mp-weixin/components/gaoyia-parse/components/wxParseVideo.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/gaoyia-parse/components/wxParseVideo"],{5020:function(n,e,t){"use strict";t.r(e);var r=t("c54e"),a=t("6c13");for(var u in a)"default"!==u&&function(n){t.d(e,n,(function(){return a[n]}))}(u);var c,o=t("f0c5"),f=Object(o["a"])(a["default"],r["b"],r["c"],!1,null,null,null,!1,r["a"],c);e["default"]=f.exports},"6c13":function(n,e,t){"use strict";t.r(e);var r=t("f5a1"),a=t.n(r);for(var u in r)"default"!==u&&function(n){t.d(e,n,(function(){return r[n]}))}(u);e["default"]=a.a},c54e:function(n,e,t){"use strict";var r;t.d(e,"b",(function(){return a})),t.d(e,"c",(function(){return u})),t.d(e,"a",(function(){return r}));var a=function(){var n=this,e=n.$createElement;n._self._c},u=[]},f5a1:function(n,e,t){"use strict";Object.defineProperty(e,"__esModule",{value:!0}),e.default=void 0;var r={name:"wxParseVideo",props:{node:{}}};e.default=r}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/gaoyia-parse/components/wxParseVideo-create-component',
+    {
+        'components/gaoyia-parse/components/wxParseVideo-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("5020"))
+        })
+    },
+    [['components/gaoyia-parse/components/wxParseVideo-create-component']]
+]);
