61b8ddba5b0ee7123e505a12d6b5b04048177f75
kenny67 <282716545@qq.com>
2021-09-09 01:28:34 +0900

    base api

--- admin/vue2/element-admin-v3/config/dev.env.js
+++ admin/vue2/element-admin-v3/config/dev.env.js
@@ -2,7 +2,8 @@ module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   SITE_NAME: '"DSSHOP电商商城"',
-  BASE_API_old: '"http://yueapps.com/api/v1/admin/"',
+  BASE_API_OLD: '"http://yueapps.com/api/v1/admin/"',
   BASE_API: '"https://wx.dg006.com/api/v1/admin/"',
+  BASE_API1: '"https://127.0.0.1/api/v1/admin/"'
 
 }
--- admin/vue2/element-admin-v3/config/prod.env.js
+++ admin/vue2/element-admin-v3/config/prod.env.js
@@ -2,5 +2,6 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   SITE_NAME: '"DSSHOP电商商城"',
-  BASE_API: '"http://dsshop.test/api/v1/admin/"',
+  BASE_API_old: '"http://dsshop.test/api/v1/admin/"',
+  BASE_API: '"https://wx.dg006.com/api/v1/admin/"',
 }
